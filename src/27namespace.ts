466d2082e76197335160ffb9001b96ef9f105758
Andry Pebrianto <andrypeb227@gmail.com>
2021-10-08 11:05:07 +0700

    Namespace

--- src/27namespace.ts
+++ src/27namespace.ts
@@ -5,11 +5,14 @@
 
 // Menggunakan namespace agar tidak error jika ada nama class yang sama
 namespace NamespaceExample {
-  class Hewan {
-    name: string = "Bambang"
+  export class Hewan {
+    name: string = "Bambang";
   }
 
   const hewan = new Hewan();
   console.log(hewan.name);
 }
 
+// Cara inisiasi class yang ada di dalam namespace
+const hewanA = new NamespaceExample.Hewan();
+console.log(hewanA);
